0fca1c9fb4b0b76f7ef8527f7f72687536f038a5
Nhat Thanh <nhatthanhtranho@gmail.com>
2025-10-11 08:24:45 +0700

    Updates

---
+++ _next/static/__nDRds3Bxqi74nyR8A8-/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/653b60f8d16dda90.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/e-reader//_next/:path+",
+        "destination": "/e-reader/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
